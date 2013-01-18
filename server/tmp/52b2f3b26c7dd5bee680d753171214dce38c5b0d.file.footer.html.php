87866e92524fb0a3da2e790361843db472ad0729
wuchang1123 <wuchang1123@163.com>
2013-01-19 03:00:09 +0800

    Revert "Revert "hbs""
    This reverts commit 11c5765270f8f16a55cd0b36b3507bf0f0b7aed8.

---
+++ server/tmp/52b2f3b26c7dd5bee680d753171214dce38c5b0d.file.footer.html.php
@@ -0,0 +1,26 @@
+<?php /* Smarty version Smarty-3.0.8, created on 2013-01-16 22:09:52
+         compiled from "/Volumes/Macintosh/project/yangjun/server/tpl/footer.html" */ ?>
+<?php /*%%SmartyHeaderCode:119263838150f6b4b05e9726-53513469%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '52b2f3b26c7dd5bee680d753171214dce38c5b0d' => 
+    array (
+      0 => '/Volumes/Macintosh/project/yangjun/server/tpl/footer.html',
+      1 => 1358343945,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '119263838150f6b4b05e9726-53513469',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+	<!-- end #page -->
+	<div id="footer">
+		<p>Copyright &copy;</p>
+	</div>
+	<!-- end #footer -->
+</body>
+</html>
